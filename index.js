ef330c12610cdf64d2eed873d3b78044f79291de
YadavBhargav <yadavbhargav.yb@gmail.com>
2021-12-31 19:31:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// let selectedColors = ['pink','red'];
+// selectedColors[2] = 4585;
+// console.log(selectedColors.length);
+
+//Performing a task
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+//Calculatin a value
+function square(number){
+    return number * number;
+}
+
+console.log(square(2));
+
