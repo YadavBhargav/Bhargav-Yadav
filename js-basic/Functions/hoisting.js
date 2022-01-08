ff77ededfdbca172dd0f85f73b3c82d36278e2b7
YadavBhargav <yadavbhargav.yb@gmail.com>
2022-01-08 21:04:35 +0530

    I have completed until rest operator part of Function (JavaScript)

---
+++ js-basic/Functions/hoisting.js
@@ -0,0 +1,15 @@
+//Function Declaration 
+
+walk();
+
+function walk(){
+    console.log('walk');
+}
+
+//Function Expression
+
+run();   //you can not declare expression function ,you will declare when finished expression function
+
+let run = function(){
+    console.log('run');
+}
