868cec7df78b750df92fc0abd2b9da40d69c8418
YadavBhargav <yadavbhargav.yb@gmail.com>
2022-01-17 21:40:54 +0530

    I have completed Arrow Functions and this part of ES6 Refresher  (ReactJs)

--- ReactJs/react-app/src/index.js
+++ ReactJs/react-app/src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello World, hiii i am developer person</h1>;
+const element = <h1>Hello World</h1>;
 ReactDOM.render(element,document.getElementById('root'));
+
+//var -> function
+// let -> block , where you can define, you can changed the value, while allready assing 
+// const -> block, not a reapet assign value 
+
+function sayHello(){
+    for(let i = 0; i < 5; i++){
+        console.log(i);
+
+        // const x = 1;
+        // x = 2; // you can not reassign constant variable
+    }
+    
+}
+sayHello();
