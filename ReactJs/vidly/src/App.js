f17ac1863a117bd11305a057b0ec4a20559725c9
YadavBhargav <yadavbhargav.yb@gmail.com>
2022-02-04 19:14:00 +0530

    I have complated Code Review part of Forms (ReactJs)

--- ReactJs/vidly/src/App.js
+++ ReactJs/vidly/src/App.js
@@ -7,7 +7,8 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import "./App.css";
 import MovieForm from "./components/movieForm";
-import LoginForm from './components/loginForm';
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   render() {
@@ -16,7 +17,8 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
-          <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
